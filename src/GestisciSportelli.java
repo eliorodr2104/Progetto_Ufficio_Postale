f931fe3330d7c963316ca43df8a6200cc301a760
elio0 <rodriguezeliomar088@gmail.com>
2022-11-03 18:46:27 +0100

    Finita classe GestisciSportelli.java con tutte le correzioni degli errori, prova a testare

--- src/GestisciSportelli.java
+++ src/GestisciSportelli.java
@@ -2,9 +2,8 @@ import java.util.ArrayList;
 
 public class GestisciSportelli {
     GestisciCode gestisciCode = new GestisciCode();
-    private ArrayList<Sportello> sportelloArrayList = new ArrayList<>();
-
-    private boolean controlloPrimaVolta;
+    public ArrayList<Sportello> sportelloArrayList = new ArrayList<>();
+    private String ticketAttuale;
 
     public GestisciSportelli(){
         this.sportelloArrayList.add(new Sportello("Antonio", "pagamento_bollettini", "", "", true));
@@ -13,46 +12,67 @@ public class GestisciSportelli {
         this.sportelloArrayList.add(new Sportello("Jose", "banco_posta", "", "", true));
         this.sportelloArrayList.add(new Sportello("Yoneikler", "servizi_postale", "", "", true));
         this.sportelloArrayList.add(new Sportello("Yeferson", "servizi_postale", "", "", true));
-        this.controlloPrimaVolta = true;
+        this.ticketAttuale = "";
     }
 
+    /**
+     * Devo rifare
+     * @param numeroSportello
+     */
     public void aggiornaStatoSportello(int numeroSportello){
         String tipologiaServizio = sportelloArrayList.get(numeroSportello).getTipologiaServizio();
-        String ticketAttuale;
 
-        if (controlloPrimaVolta && !gestisciCode.isEmpty(tipologiaServizio)){
-            sportelloArrayList.get(numeroSportello).setSportelloLibero(false);
+        if (!gestisciCode.isEmpty(tipologiaServizio)){
+            if (sportelloArrayList.get(numeroSportello).getNumeroTicket().equals("")){
+                sportelloArrayList.get(numeroSportello).setSportelloLibero(false);
 
-            sportelloArrayList.get(numeroSportello).setNumeroTicket(gestisciCode.pop(tipologiaServizio).getTicket());
+                sportelloArrayList.get(numeroSportello).setNumeroTicket(gestisciCode.pop(tipologiaServizio).getTicket());
 
-            sportelloArrayList.get(numeroSportello).setNumeroProgressivo(gestisciCode.pop(tipologiaServizio).getTicket());
+                if (!gestisciCode.isEmpty(tipologiaServizio))
+                    sportelloArrayList.get(numeroSportello).setNumeroProgressivo(gestisciCode.pop(tipologiaServizio).getTicket());
 
-            controlloPrimaVolta = false;
+            }else {
+
+                ticketAttuale = sportelloArrayList.get(numeroSportello).getNumeroProgressivo();
+
+                if (!ticketAttuale.equals("")){
+
+                    sportelloArrayList.get(numeroSportello).setSportelloLibero(false);
+
+                    sportelloArrayList.get(numeroSportello).setNumeroTicket(ticketAttuale);
+
+                    sportelloArrayList.get(numeroSportello).setNumeroProgressivo(gestisciCode.pop(tipologiaServizio).getTicket());
+
+                }else {
+                    sportelloArrayList.get(numeroSportello).setSportelloLibero(false);
+
+                    sportelloArrayList.get(numeroSportello).setNumeroProgressivo(gestisciCode.pop(tipologiaServizio).getTicket());
+                }
+            }
+
+        }else{
+            if (gestisciCode.pop(tipologiaServizio) == null){
+                if (!sportelloArrayList.get(numeroSportello).getNumeroProgressivo().equals("")){
+                    ticketAttuale = sportelloArrayList.get(numeroSportello).getNumeroProgressivo();
+
+                    sportelloArrayList.get(numeroSportello).setNumeroTicket(ticketAttuale);
+
+                    sportelloArrayList.get(numeroSportello).setNumeroProgressivo("");
+
+                }else {
+                    sportelloArrayList.get(numeroSportello).setNumeroTicket("");
+
+                    sportelloArrayList.get(numeroSportello).setSportelloLibero(true);
+                }
+            }
         }
-
-        if (gestisciCode.isEmpty(tipologiaServizio)){
-            sportelloArrayList.get(numeroSportello).setSportelloLibero(true);
-
-        }else {
-            ticketAttuale = sportelloArrayList.get(numeroSportello).getNumeroProgressivo();
-
-            sportelloArrayList.get(numeroSportello).setSportelloLibero(false);
-
-            sportelloArrayList.get(numeroSportello).setNumeroTicket(ticketAttuale);
-
-            sportelloArrayList.get(numeroSportello).setNumeroProgressivo(gestisciCode.pop(tipologiaServizio).getTicket());
-        }
-
     }
 
-    public void aggiungerePersonaCoda(int numeroSportello, String codiceFiscale, String nome, String cognome, String tipoServizio, String ticket){
-        String tipologiaServizio = sportelloArrayList.get(numeroSportello).getTipologiaServizio();
-
+    public void aggiungerePersonaCoda(String tipologiaServizio, String codiceFiscale, String nome, String cognome, String tipoServizio, String ticket){
         gestisciCode.push(tipologiaServizio, new Cliente(codiceFiscale, nome, cognome, tipoServizio, ticket));
     }
 
     public boolean controlloSportello(int numeroSportello){
         return sportelloArrayList.get(numeroSportello).isSportelloLibero();
     }
-
 }
--- src/InterfacciaSportelli.java
+++ src/InterfacciaSportelli.java
@@ -1,2 +1,198 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class InterfacciaSportelli {
+
+    GestisciSportelli g1 = new GestisciSportelli();
+
+    private int scelta, indiceSportello, contaBancoPosta, contaServiziPostale, contaPagamentoBollettini;
+    private final String regexCodiceFiscale, regexNomeCognome;
+    private String codiceFiscale, nome, cognome, tipoServizio, ticket;
+
+    public InterfacciaSportelli(){
+        this.scelta = 0;
+        this.indiceSportello = 0;
+        this.regexCodiceFiscale = "[a-zA-Z]{6}\\d\\d[a-zA-Z]\\d\\d[a-zA-Z]\\d\\d\\d[a-zA-Z]";
+        this.regexNomeCognome = "[a-zA-Z]*";
+    }
+
+    public int stampaSportelli(){
+        String[] tipologiaServizio;
+        int ultimoIndice = 0;
+
+        char oldChar, newChar;
+
+        for (int i = 0 ; i < g1.sportelloArrayList.size() ; i++){
+            tipologiaServizio = g1.sportelloArrayList.get(i).getTipologiaServizio().split("_");
+
+            oldChar = tipologiaServizio[0].charAt(0);
+
+            newChar = Character.toUpperCase(oldChar);
+
+            tipologiaServizio[0] = tipologiaServizio[0].replace(oldChar, newChar);
+
+            System.out.println(i + 1 + ". Lavoratore: " + g1.sportelloArrayList.get(i).getNomeLavoratore() +
+                    ", Tipo sportello: " + tipologiaServizio[0] + " " + tipologiaServizio[1] + ".");
+
+            ultimoIndice = i + 1;
+        }
+
+        return ultimoIndice;
+    }
+
+    public void menu(){
+        int numeroMassimoSportelli;
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println("Seleziona il tuo sportello: ");
+        numeroMassimoSportelli = stampaSportelli();
+
+        do {
+            try {
+                indiceSportello = Integer.parseInt(s1.next());
+
+                if (indiceSportello < 0 || indiceSportello > numeroMassimoSportelli)
+                    System.out.println("Valore inserito non valido");
+
+            }catch (NumberFormatException e){
+                indiceSportello = -10;
+                System.out.println("Valore inserito non valido");
+            }
+        }while (indiceSportello < 0 || indiceSportello > numeroMassimoSportelli);
+
+        System.out.println("Scegli l'operazione che desideri fare: \n" +
+                "1.Aggiorna lo stato del tuo sportello.\n" +
+                "2.Aggiungi una persona alla coda.\n" +
+                "3.Controlla lo stato del tuo sportello.");
+
+        do {
+            try{
+                scelta = Integer.parseInt(s1.next());
+
+                if (scelta < 1 || scelta > 3)
+                    System.out.println("Valore inserito non valido");
+
+            }catch (NumberFormatException e){
+                scelta = -10;
+                System.out.println("Valore inserito non valido");
+            }
+        }while (scelta < 1 || scelta > 3);
+
+        switch (scelta){
+            case 1 ->{
+                g1.aggiornaStatoSportello(indiceSportello - 1);
+            }
+
+            case 2 ->{
+                System.out.println("Inserisci il codici fiscale della persona: ");
+
+                //Esempio di codice fiscale valido: HYHLHL78K62M873H
+                do {
+                    codiceFiscale = s1.next();
+
+                    if (!Pattern.matches(regexCodiceFiscale, codiceFiscale))
+                        System.out.println("Codice fiscale non valido, reinserire!!");
+
+                }while (!Pattern.matches(regexCodiceFiscale, codiceFiscale));
+
+                System.out.println("Inserisci il nome della persona");
+
+                do {
+                    nome = s1.next();
+
+                    if (!Pattern.matches(regexNomeCognome, nome))
+                        System.out.println("Nome non valido, reinserire!!");
+
+                }while (!Pattern.matches(regexNomeCognome, nome));
+
+                System.out.println("Inserisci il cognome della persona");
+
+                do {
+                    cognome = s1.next();
+
+                    if (!Pattern.matches(regexNomeCognome, cognome))
+                        System.out.println("Cognome non valido, reinserire!!");
+
+                }while (!Pattern.matches(regexNomeCognome, cognome));
+
+                System.out.println("Selezione il tipo di servizio: \n" +
+                        "1.Banco posta.\n" +
+                        "2.Servizi postale.\n" +
+                        "3.Pagamento bollettini.");
+
+                do {
+                    try{
+                        scelta = Integer.parseInt(s1.next());
+
+                        if (scelta < 1 || scelta > 3)
+                            System.out.println("Valore inserito non valido");
+
+                    }catch (NumberFormatException e){
+                        scelta = -10;
+                        System.out.println("Valore inserito non valido");
+                    }
+                }while (scelta < 1 || scelta > 3);
+
+                switch (scelta){
+                    case 1 -> {
+                        tipoServizio = "banco_posta";
+
+                        if (contaBancoPosta > 999)
+                            contaBancoPosta = 0;
+
+                        ticket = "b" + contaBancoPosta;
+                        contaBancoPosta++;
+                    }
+                    case 2 -> {
+                        tipoServizio = "servizi_postale";
+
+                        if (contaServiziPostale > 999)
+                            contaServiziPostale = 0;
+
+                        ticket = "s" + contaServiziPostale;
+                        contaServiziPostale++;
+                    }
+                    case 3 -> {
+                        tipoServizio = "pagamento_bollettini";
+
+                        if (contaPagamentoBollettini > 999)
+                            contaPagamentoBollettini = 0;
+
+                        ticket = "p" + contaPagamentoBollettini;
+                        contaPagamentoBollettini++;
+                    }
+                }
+
+                g1.aggiungerePersonaCoda(tipoServizio, codiceFiscale, nome, cognome, tipoServizio, ticket);
+            }
+
+            case 3 -> {
+                if (!g1.controlloSportello(indiceSportello -1)){
+                    System.out.println("Il ticket del cliente attuale è: " + g1.sportelloArrayList.get(indiceSportello -1).getNumeroTicket());
+
+                    System.out.println("Il ticket del prossimo cliente è: " + g1.sportelloArrayList.get(indiceSportello -1).getNumeroProgressivo());
+
+                }else
+                    System.out.println("Sportello vuoto");
+            }
+        }
+
+        System.out.println("Inserisci \"1\" per continuare o \"2\" per chiudere il programma");
+
+        do {
+            try{
+                scelta = Integer.parseInt(s1.next());
+
+                if (scelta < 1 || scelta > 3)
+                    System.out.println("Valore inserito non valido");
+
+            }catch (NumberFormatException e){
+                scelta = -10;
+                System.out.println("Valore inserito non valido");
+            }
+        }while (scelta < 1 || scelta > 2);
+
+        if (scelta == 1)
+            menu();
+    }
 }
